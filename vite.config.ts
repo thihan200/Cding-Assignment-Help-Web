0ac269c2257541956d077e0eea28da3d9a7f5438
Thihan Gamage <71422014+thihan200@users.noreply.github.com>
2026-05-27 23:13:50 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/Cding-Assignment-Help-Web/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
